2c8587dbf4633a46f4f6eb0e962e548b719758b8
sachin <sangwansachin631@gmail.com>
2024-09-16 10:00:18 +0530

    update

--- server/router/user.auth.routes.js
+++ server/router/user.auth.routes.js
@@ -1,4 +1,4 @@
-const  { user_register, user_login, user_logout, user_list, user_details, chatHistory, create_group } =require("../controller/user.auth.controller");
+const  { user_register, user_login, user_logout, user_list, user_details, chatHistory, create_group, get_my_chat, get_my_groups, add_member_to_group } =require("../controller/user.auth.controller");
 const express = require("express");
 const { login_middleware } = require("../middlewares/login.middlewares")
 const user_auth_router = express.Router();
@@ -10,7 +10,12 @@ user_auth_router.get('/user/user_list',user_list);
 user_auth_router.get('/user/details',login_middleware,user_details) ;
 // user_auth_router.get('/chat/history/:userId',login_middleware,chatHistory)
 //chat module
-user_auth_router.post('/user/create_group',login_middleware,create_group)
+user_auth_router.post('/user/create_group',login_middleware,create_group);
+user_auth_router.get('/user/get_my_chat',login_middleware,get_my_chat);
+user_auth_router.get('/user/get_my_group',login_middleware,get_my_groups);
+user_auth_router.put('/user/add_member_to_group',login_middleware,add_member_to_group);
+
+
 
 
 module.exports= user_auth_router;
